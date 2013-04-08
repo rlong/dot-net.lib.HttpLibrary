0522b7a7b6112861efe9eb8309bb3f4af0a354fe
Richard Long <rlong@nowhere>
2013-04-08 10:09:21 +1000

    initial commit of 'service'

---
+++ service/test/TestService.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) 2013 Richard Long & HexBeerium
+//
+// Released under the MIT license ( http://opensource.org/licenses/MIT )
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using jsonbroker.library.server.broker;
+using jsonbroker.library.common.log;
+using jsonbroker.library.common.broker;
+using jsonbroker.library.common.exception;
+using jsonbroker.library.common.json;
+
+namespace jsonbroker.library.service.test
+{
+    public class TestService : DescribedService
+    {
+        
+        private static readonly Log log = Log.getLog(typeof(TestService));
+
+        public static readonly String SERVICE_NAME = "jsonbroker.TestService";
+        public static readonly ServiceDescription SERVICE_DESCRIPTION = new ServiceDescription(SERVICE_NAME);
+
+
+
+
+        public JsonObject echo(JsonObject associativeParamaters)
+        {
+
+            log.enteredMethod();
+            return associativeParamaters;
+        }
+
+        public void ping()
+        {
+            log.enteredMethod();            
+        }
+
+        private String ToString(Object o)
+        {
+            return o.ToString();
+        }
+
+        public void raise_error()
+        {
+
+            log.enteredMethod();
+            BaseException e = new BaseException(this, "TestService.raise_error() called");
+            e.ErrorDomain = "jsonbroker.TestService.RAISE_ERROR";
+            throw e;            
+        }
+
+
+
+        public BrokerMessage process(BrokerMessage request)
+        {
+            String methodName = request.getMethodName();
+
+
+            if ("echo".Equals(methodName))
+            {
+
+                JsonObject associativeParamaters = request.getAssociativeParamaters();
+
+                associativeParamaters = this.echo(associativeParamaters);
+
+                BrokerMessage answer = BrokerMessage.buildResponse(request);
+                answer.setAssociativeParamaters(associativeParamaters);
+                return answer;
+            }
+
+            if ("ping".Equals(methodName))
+            {
+                this.ping();
+
+                BrokerMessage response = BrokerMessage.buildResponse(request);
+                return response;
+            }
+
+            if ("raiseError".Equals(methodName))
+            {
+                this.raise_error();
+
+                BrokerMessage response = BrokerMessage.buildResponse(request);
+                return response;
+            }
+
+            throw ServiceHelper.methodNotFound(this, request);
+
+        }
+
+
+        public ServiceDescription getServiceDescription()
+        {
+            return SERVICE_DESCRIPTION;
+        }
+
+
+    }
+}
---
+++ service/test/TestServiceIntegrationTest.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2013 Richard Long & HexBeerium
+//
+// Released under the MIT license ( http://opensource.org/licenses/MIT )
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using jsonbroker.library.common.log;
+using jsonbroker.library.server.broker;
+using jsonbroker.library.common.auxiliary;
+
+namespace jsonbroker.library.service.test
+{
+
+
+    [TestFixture]
+    [Category("integration")]
+    public class TestServiceIntegrationTest
+    {
+
+        private static Log log = Log.getLog(typeof(TestServiceIntegrationTest));
+
+        private static TestServiceProxy _proxy;
+
+        static TestServiceIntegrationTest() 
+        {
+            DescribedService namedservice = new TestService();
+
+            Service service = IntegrationTestUtilities.setupService(IntegrationTestUtilities.TestType.EMBEDDED_AUTH_SERVER, namedservice);
+
+            _proxy = new TestServiceProxy(service);
+
+        }
+
+        [Test]
+        public void test1()
+        {
+            log.debug("test1");
+        }
+
+        [Test]
+        public void testPing()
+        {
+            _proxy.ping();
+        }
+
+        [TestFixtureTearDown]
+        public void fixtureTearDown()
+        {
+            log.enteredMethod();
+        }
+
+    }
+}
